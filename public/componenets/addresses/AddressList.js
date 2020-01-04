9f314e553eac91cd09a7ff83c27b60e258190031
drmeloy <drmeloy@gmail.com>
2020-01-03 17:58:05 -0700

    empty address book notification

--- public/componenets/addresses/AddressList.js
+++ public/componenets/addresses/AddressList.js
@@ -5,14 +5,25 @@ import AddressEntry from './AddressEntry.js';
 class AddressList extends Componenet {
   onRender(dom){
     const addressList = dom.querySelector('#address-list');    
-
     fetchAddresses()
       .then(fetchedAddresses => {
-        fetchedAddresses.forEach(address => {
-          const props = { address };
-          const addressEntry = new AddressEntry(props);
-          addressList.appendChild(addressEntry.renderDOM());
-        });  
+        console.log(fetchedAddresses);
+        
+        if(fetchedAddresses.length < 1){
+          const emptyNotification = document.createElement('div');
+          emptyNotification.classList.add('empty-notification');
+          emptyNotification.innerHTML = /*html*/ `
+            <h1>Oops!</h1>
+            <p>It looks like you don't have any addresses added to your Address Book yet. Follow the "Add An Entry" link above to add your first address!</p>
+          `;
+          addressList.appendChild(emptyNotification);
+        } else {
+          fetchedAddresses.forEach(address => {
+            const props = { address };
+            const addressEntry = new AddressEntry(props);
+            addressList.appendChild(addressEntry.renderDOM());
+          }); 
+        }
       });
       
   }
